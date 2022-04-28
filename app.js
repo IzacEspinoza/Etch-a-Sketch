9208198169ffc7b6202cc9d3b876977b737033c0
Izac <izikki11499@gmail.com>
2022-04-27 17:59:02 -0700

    Created 16x16 grid of divs ussing js, styled it a bit to see what we dealing with

---
+++ app.js
@@ -0,0 +1,24 @@
+//reference our container in our html
+const container = document.getElementById("container");
+
+//function makes the grid pretty much, with specified dimensions
+function makeRows(rows, cols) {
+    /* essentially, 
+     * grabs the container, 
+     *makes a new css rule for specified prop, 
+     *sets it to the parameter values
+    */
+  container.style.setProperty('--grid-rows', rows);
+  container.style.setProperty('--grid-cols', cols);
+
+  //loop, pretty much creates the grid,
+  //making a div until the area we want for the grid is reached
+  for (c = 0; c < (rows * cols); c++) {
+    let cell = document.createElement("div");
+    cell.innerText = (c + 1);//sets some text inside, optional
+    container.appendChild(cell).className = "grid-item";//appends to container, and gives it a class name
+  };
+};//end function
+
+//create the grid we want
+makeRows(16, 16);
