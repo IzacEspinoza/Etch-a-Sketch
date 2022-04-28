ba18a8f4441eb3a74c3f60f1a58cef84e5ce60cf
Izac <izikki11499@gmail.com>
2022-04-27 19:07:56 -0700

    Created the basic hover/drawing effect on the grid boxes in app.js

--- app.js
+++ app.js
@@ -22,3 +22,13 @@ function makeRows(rows, cols) {
 
 //create the grid we want
 makeRows(16, 16);
+
+
+//add event listener to the grid-items,
+//so when we hover over the grid-item, the background color changes(drawing)
+const divs = document.querySelectorAll('.grid-item');
+divs.forEach(item => {
+    item.addEventListener('mouseover', function(){
+        item.style.backgroundColor = 'blueviolet';//change this later to be a color chosen by user
+    })//end event listener
+})//end foreach
