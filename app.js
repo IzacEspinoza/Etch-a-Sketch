3252b0b124de8091f834980d109b15643f5caf89
Izac <izikki11499@gmail.com>
2022-05-01 12:48:27 -0700

    little changes

--- app.js
+++ app.js
@@ -1,70 +1,51 @@
 //DEFAULT grid size
 const DEFAULT_GRID = 16;
-
-
-
-//reference our container in our html
+//container div
 const container = document.getElementById("container");
+//New sketch button
+const button  = document.querySelector('#set-grid');
+
 
 //create the default grid(16x16)
 makeRows(DEFAULT_GRID);
 
-//make button, 
-//when we click, prompt(set dimensions of grid)
-//call makeRows(*with values entered)
-//reference our button
-const button  = document.querySelector('#set-grid');
-//add an event listener to this button, so 
+//on click, clears grid and makes new one based on users input
 button.addEventListener('click', function(){ 
-
-   //prompt user for new grid size
    let newSize = prompt("What size grid would you like?(enter one number)");
-   //clear current grid
    clearGrid();
    //call makeRows with this value
    //makeRows(newSize);
-   
-
 });
 
-//clear grid
-// function that clears the grid
+//clear the current grid
 function clearGrid(){
- //remove the painting
- let allCells = document.querySelectorAll('.grid-item').forEach(cell => {
-  cell.style.backgroundColor = "white";
-  
+ //sets all the grid items colors back to white
+ let allCells = document.querySelectorAll('.grid-item');
+ allCells.forEach(cell => {
+    cell.style.backgroundColor = "white";
  })
 };
 
-  
-
-//add event listener to the grid-items,
-//so when we hover over the grid-item, the background color changes(drawing)
+//this is what allows the user to 'draw'
 const divs = document.querySelectorAll('.grid-item');
 divs.forEach(item => {
     item.addEventListener('mouseover', function(){
         item.style.backgroundColor = 'blueviolet';//change this later to be a color chosen by user
-    })//end event listener
-})//end foreach
+    })//end mouseover
+})//end forEach
 
-//function makes the grid pretty much, with specified dimensions
-function makeRows(/*rows, cols*/squares) {
-    /* essentially, 
-     * grabs the container, 
-     *makes a new css rule for specified prop, 
-     *sets it to the parameter values
-    */
+//Make grid with users input for dimensions
+function makeRows(squares) {
+
+  //get the containers styling, make a new css rule and set it to value
   container.style.setProperty('--grid-rows', /*rows*/squares);
   container.style.setProperty('--grid-cols', /*cols*/squares);
-
-  //loop, pretty much creates the grid,
   //making a div until the area we want for the grid is reached
   for (c = 0; c < (/*rows*/ squares*squares /*cols*/); c++) {
     let cell = document.createElement("div");
     //cell.innerText = (c + 1);//sets some text inside, optional
     container.appendChild(cell).className = "grid-item";//appends to container, and gives it a class name
-  };
+  };//end for loop
 };//end function
 
 
